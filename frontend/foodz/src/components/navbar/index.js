d51c14e36a709a6f16c95d522c7be22b38ccf171
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-08-03 14:42:54 +0100

    delete react-bootstrap use only bootstrap

---
+++ frontend/foodz/src/components/navbar/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+import './navbar.css'
+
+
+function NavBar(){
+    return (
+        <div className="pos-f-t">
+        <div className="collapse" id="navbarToggleExternalContent">
+          <div className="bg-dark p-4">
+            <h5 className="text-white h4">Collapsed content</h5>
+            <span className="text-muted">Toggleable via the navbar brand.</span>
+          </div>
+        </div>
+        <nav className="navbar navbar-dark bg-dark">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      </div>
+    );
+}
+
+
+
+export default NavBar;
--- frontend/foodz/src/domain/home/index.js
+++ frontend/foodz/src/domain/home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../../assets/images/logo.svg';
 import './home.css';
-
+import NavBar from '../../components/navbar/index'
 function Home() {
   return (
     <div className="App">
+        <NavBar></NavBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
