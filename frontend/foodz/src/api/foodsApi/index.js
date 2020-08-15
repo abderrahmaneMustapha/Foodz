c2313c0c9f5e70062a76a9a71c9bbfdccb27ff0b
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-08-15 06:58:51 +0100

    setting up project for redux

---
+++ frontend/foodz/src/api/foodsApi/index.js
@@ -0,0 +1,87 @@
+let foods =  [{
+    title: "Food1",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+}, {
+    title: "Food2",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food3",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food4",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food1",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+}, {
+    title: "Food2",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food3",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food4",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food1",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+}, {
+    title: "Food2",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food3",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food4",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food1",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+}, {
+    title: "Food2",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food3",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+},{
+    title: "Food4",
+    photo : "https://hips.hearstapps.com/vidthumb/images/delish-keto-crack-chicken-still003-1553543868.jpg?crop=1.00xw:0.891xh;0,0.0571xh&resize=480:*",
+    description : "Take online courses in Docker from top universities and institutions to improve your web application and software development skills. Learn docker, dockerfiles",
+
+}]
+
+fetchFoods =  () =>{
+    return foods
+}
+
+export default fetchFoods
--- frontend/foodz/src/domain/restaurant/index.js
+++ frontend/foodz/src/domain/restaurant/index.js
@@ -1,15 +1,23 @@
 import React from "react"
 
 
+// outer components
+import WNavBar from '../../components/navbars/whitenavbar/index'
+
 class Restaurant extends  React.Component{
 
 
     render(){
 
         return(
+            <>
+            <header>
+                <WNavBar city={this.state.city} />
+            </header>
             <div>
                 Restaurant page 
             </div>
+            </>
         )
     }
 }
