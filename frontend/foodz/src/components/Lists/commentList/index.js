3165b5d92effd76ba51cd19631b9c4547c757e84
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-08-24 12:24:31 +0100

    one component for existing and new comment

--- frontend/foodz/src/components/Lists/commentList/index.js
+++ frontend/foodz/src/components/Lists/commentList/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./comment.css"
-import {MainExistingComment, ReplyExistingComment } from "../../inputs/commentTextArea/index"
+import {ExistingComment } from "../../inputs/commentTextArea/index"
 class CommentList extends React.Component{
 
     constructor(props){
@@ -16,8 +16,8 @@ class CommentList extends React.Component{
                     this.state.commentList.map( 
                     (element, index)=>(
                     <li  key = {index} className="list-group-item">
-                        <MainExistingComment
-                           
+                        <ExistingComment
+                           classPlus = "main-exisiting-comment-container"
                             text={element.text} 
                             username={element.username} 
                             photo={element.photo}
@@ -26,7 +26,8 @@ class CommentList extends React.Component{
                         <ul  className="list-group comment-list-reply">
                         {element.replys.map((subelement, index)=>(
                             <li key={"AZER"+index}  className="list-group-item">
-                            <ReplyExistingComment
+                            <ExistingComment
+                                classPlus = "reply-exisiting-comment-container"
                                 text={subelement.text} 
                                 username={subelement.username} 
                                 photo={subelement.photo}
--- frontend/foodz/src/components/inputs/commentTextArea/index.js
+++ frontend/foodz/src/components/inputs/commentTextArea/index.js
@@ -2,19 +2,16 @@ import React from "react"
 import "./comment.css"
 import StarRatings from 'react-star-ratings';
 
-class MainExistingComment extends  React.Component {
+class ExistingComment extends  React.Component {
         constructor(props){
             super(props)
-            this.handleReply = this.handleReply.bind()
         }
 
-        handleReply  = (event)=>{
-            console.log(event.target.value)
-        }
+    
 
         render(){
             return(
-                <div  key={this.props.key} className="container comment main-exisiting-comment-container">
+                <div  key={this.props.key} className={"container comment "+this.props.classPlus}>
                 <div className="row">
                     <div className="col-md-1 col-sm-4">
                         <img className="rounded-circle" src={this.props.photo}></img>
@@ -22,6 +19,7 @@ class MainExistingComment extends  React.Component {
                     <div className=" col-md-11 col-sm-8">
                         <header  className="d-md-flex">
                         <h5 className="font-weight-bold mr-4">{this.props.username}</h5>
+                        { this.props.review ?
                         <StarRatings 
                                         rating={parseFloat(this.props.review)}
                                         starRatedColor="#FFBF00"
@@ -29,7 +27,9 @@ class MainExistingComment extends  React.Component {
                                         name='rating'
                                         starDimension="1.4em"
                                         starSpacing="1px"
-                        s></StarRatings>
+                        ></StarRatings>:
+                        <></>
+                        }
                         </header>
                         <div className="font-weight-light m-2">{this.props.text}</div>
                         <div  className="comment-utils d-flex flex-row align-items-center mt-3">
@@ -58,41 +58,6 @@ class MainExistingComment extends  React.Component {
 
 }
 
-class ReplyExistingComment extends React.Component{
-    render(){
-        return(
-            <div  className=" comment reply-exisiting-comment-container container">
-            <div className="row">
-                <div className="col-md-1 col-sm-3">
-                    <img className="rounded-circle" src={this.props.photo}></img>
-                </div>
-                <div className=" col-md-11 col-sm-8">
-                    <header  className="d-md-flex">
-                    <h5 className="font-weight-bold mr-4">{this.props.username}</h5>
-                    </header>
-                    <div className="font-weight-light m-2">{this.props.text}</div>
-                    <div  className=" comment-utils d-flex flex-row align-items-center mt-3">
-                        <div title="up vote">
-                            <i className="fas fa-chevron-up"></i>
-                            <span className="text-muted">33 </span>
-                        </div> 
-                        . 
-                        <div title="down vote">
-                            <i  className="fas fa-chevron-down"></i>
-                            <span className="text-muted">33 </span>
-                        </div>
-                        . 
-                        <div title="reply">
-                            <i className="fas fa-reply"></i>
-                        </div>
-                    </div>                        
-                </div>
-            </div>                   
-                
-            </div>
-        )
-       
-    }
-}
-export {MainExistingComment, ReplyExistingComment }
+
+export {ExistingComment }
 
