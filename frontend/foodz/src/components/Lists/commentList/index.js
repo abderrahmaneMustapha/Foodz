844c6ab726804c8423a7a1b86c7a8a8c870a819a
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-08-28 14:05:31 +0100

    users can add new comment

--- frontend/foodz/src/components/Lists/commentList/index.js
+++ frontend/foodz/src/components/Lists/commentList/index.js
@@ -1,6 +1,11 @@
 import React from "react"
 import "./comment.css"
-import {ExistingComment } from "../../inputs/commentTextArea/index"
+import ReactDOMServer from 'react-dom/server';
+import {ExistingComment,NewComment } from "../../inputs/commentTextArea/index"
+let current_username  = "Mustapha"
+let current_userimage = "https://scontent-mrs2-1.xx.fbcdn.net/v/t1.0-1/p240x240/91818612_100248741644513_2244727394118139904_n.jpg?_nc_cat=109&_nc_sid=dbb9e7&_nc_ohc=Jh8mckF5fyYAX889OWa&_nc_ht=scontent-mrs2-1.xx&tp=6&oh=3809d73d2fd0904a2ae191f08e2ead08&oe=5F6DEDF1"
+
+
 class CommentList extends React.Component{
 
     constructor(props){
@@ -8,10 +13,48 @@ class CommentList extends React.Component{
         this.state = {
             commentList : this.props.commentList
         }
+        
+        this.handleAddComment = this.handleAddComment.bind()
     }
+
+    handleAddComment = (event)=>{
+        event.preventDefault()
+        // convert string to html node
+        console.log("firsr to ttry this weird thing")
+        var stringToHTML = function (str) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(str, 'text/html');
+            return doc.body.firstChild;
+        };
+
+        // this is the hole comment list container
+        let ul = document.getElementById('comment-list')
+        // get the new comment from the text area comment
+        let textarea = document.getElementById('new-comment-textarea').value
+        
+        // 
+        // get  current review
+        // by getting the title of the star-ratings container 
+        // this title will alawys contain the number of stars in the first case
+        let current_review  = document.getElementById('new-comment-container').getElementsByClassName('star-ratings')[0].title[0]
+        console.log(current_review)
+        let tempCommentList = this.state.commentList 
+        tempCommentList.push({
+            text: textarea, username : current_username, photo : current_userimage, review : current_review, replys :[]
+        })
+        console.log(tempCommentList)
+        
+        this.setState({
+            commentList : [...tempCommentList]
+        })
+
+    }
+
     render(){
         return(
             <ul  id="comment-list" className="list-group">
+                <NewComment myOnClick={this.handleAddComment}></NewComment>
+                
                 {
                     this.state.commentList.map( 
                     (element, index)=>(
@@ -23,19 +66,24 @@ class CommentList extends React.Component{
                             photo={element.photo}
                             review={element.review}
                         />
-                        <ul  className="list-group comment-list-reply">
-                        {element.replys.map((subelement, index)=>(
-                            <li key={"AZER"+index}  className="list-group-item">
-                            <ExistingComment
-                                classPlus = "reply-exisiting-comment-container"
-                                text={subelement.text} 
-                                username={subelement.username} 
-                                photo={subelement.photo}
-                             />
-                            </li> 
-                        )                             
-                        )}
-                        </ul>
+                        
+                            <ul  className="list-group comment-list-reply">
+                                
+                                    { element.replys.map((subelement, index)=>(
+                                        <li key={"AZER"+index}  className="list-group-item">
+                                        <ExistingComment
+                                            classPlus = "reply-exisiting-comment-container"
+                                            text={subelement.text} 
+                                            username={subelement.username} 
+                                            photo={subelement.photo}
+                                        />
+                                        </li> 
+                                    )                             
+                                    )}
+                                
+                            </ul> 
+                        
+                     
                     </li>
                     )                  
                     )
--- frontend/foodz/src/components/inputs/commentTextArea/index.js
+++ frontend/foodz/src/components/inputs/commentTextArea/index.js
@@ -2,8 +2,58 @@ import React from "react"
 import "./comment.css"
 import StarRatings from 'react-star-ratings';
 
+
 let current_username  = "Mustapha"
 let current_userimage = "https://scontent-mrs2-1.xx.fbcdn.net/v/t1.0-1/p240x240/91818612_100248741644513_2244727394118139904_n.jpg?_nc_cat=109&_nc_sid=dbb9e7&_nc_ohc=Jh8mckF5fyYAX889OWa&_nc_ht=scontent-mrs2-1.xx&tp=6&oh=3809d73d2fd0904a2ae191f08e2ead08&oe=5F6DEDF1"
+
+class NewComment extends React.Component{
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            rating : 3
+        }
+
+        
+        this.handleChangeRating = this.handleChangeRating.bind()
+    }
+
+    handleChangeRating = (newRating)=>{
+        console.log(newRating)
+        this.setState({
+            rating: newRating
+        });
+    }
+
+
+
+    render(){
+        return(
+            <form id="new-comment-container" className="w-100">
+                <header>                
+                    <h5>Add new comment : </h5>
+                    <StarRatings 
+                        starHoverColor = "#FFBF00"
+                        starRatedColor="#FFBF00"
+                        rating={this.state.rating}
+                        numberOfStars={5}
+                        changeRating={rating => this.handleChangeRating(rating)}
+                        name='new rating'
+                        starDimension="1.7em"
+                        starSpacing="1px"
+                    ></StarRatings>
+                </header>
+                
+                <textarea id="new-comment-textarea" className="w-100 my-3" placeholder="add new comment" required={true}>
+
+                </textarea>
+                <button className="btn btn-success" onClick={this.props.myOnClick}>add comment</button>
+            </form>
+        )
+    }
+}
+
+
 class ExistingComment extends  React.Component {
         constructor(props){
             super(props)
@@ -11,10 +61,19 @@ class ExistingComment extends  React.Component {
 
     
         handleReply = (event)=>{
+            
             let main_comment_li  = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
             let main_comment_replys = main_comment_li.getElementsByClassName('comment-list-reply')[0]
-
-            if(!main_comment_replys.firstChild.className.includes("comment-temp-add")){
+            let can_reply = (main_comment_replys)=>{
+                if(main_comment_replys.firstChild){
+                    if (!main_comment_replys.firstChild.className.includes("comment-temp-add")) return true
+                    else return false
+                }
+                else{
+                    return true
+                }
+            }
+            if(can_reply){
                 
                 // create new element in the list 
                 // by cloning the first child (li) of the replys list
@@ -41,18 +100,26 @@ class ExistingComment extends  React.Component {
                 share_button.addEventListener("click", event=>{
                     share_button.replaceWith(comment_utils)
                     comment_text_area.contentEditable = false
+                    list_element.classList.remove('comment-temp-add')
                 })
                 list_element.getElementsByClassName('col-md-11')[0].appendChild(share_button)
 
-                // remove the comment utils            
-                main_comment_replys.insertBefore(list_element, main_comment_replys.firstChild);
+                // remove the comment utils  
+                if (main_comment_replys.firstChild) {
+                    main_comment_replys.insertBefore(list_element, main_comment_replys.firstChild);
+                }  
+                else{
+                    main_comment_replys.appendChild(list_element) 
+                } 
+                    
+                
             }
            
-      
         }
+
         render(){
             return(
-                <div  key={this.props.key} className={"container comment "+this.props.classPlus}>
+                <div  key={this.props.key} className={"bg-white container comment "+this.props.classPlus}>
                 <div className="row">
                     <div className="col-md-1 col-sm-4">
                         <img className="rounded-circle" src={this.props.photo}></img>
@@ -83,10 +150,12 @@ class ExistingComment extends  React.Component {
                                 <i  className="fas fa-chevron-down"></i>
                                 <span className="text-muted">33 </span>
                             </div>
-                            . 
+                            { this.props.review ?
                             <div onClick={this.handleReply} title="reply">
                                 <i className="fas fa-reply"></i>
-                            </div>
+                            </div>:
+                            <></>
+                            }
                             <span className="ml-auto"> see all </span>
                         </div>                        
                     </div>
@@ -100,5 +169,5 @@ class ExistingComment extends  React.Component {
 }
 
 
-export {ExistingComment }
+export {ExistingComment, NewComment }
 
