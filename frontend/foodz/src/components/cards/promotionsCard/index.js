aaad6f58dc74a34bc5121df02d2fa80ef5fbf984
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-09-01 12:09:40 +0100

    modal show photos onclick and / we can close modals

--- frontend/foodz/src/components/cards/promotionsCard/index.js
+++ frontend/foodz/src/components/cards/promotionsCard/index.js
@@ -6,15 +6,29 @@ import Modal from "./modal/index"
 class Card extends React.Component{
   constructor(props){
     super(props)
-    
+    this.state = {
+      isopen : false
+    }
+    this.handleOpen = this.handleOpen.bind()
   }
-  
+  handleOpen = (event)=>{
+    this.setState({
+        isopen:true
+    })
+  }
+
+  handleClose = (event)=>{
+    this.setState({
+        isopen:false
+    })      
+  }
+
+
     render(){
         return(
           <>
             <div  
-              
-              data-toggle="modal" 
+              onClick={this.handleOpen}
               data-target={"#modal-promotion-"+this.props.index}   
               className={`promotions-card  mb-2 px-1  bg-transparent ${this.props.md} ${this.props.sm} `}>
               <div className="card  bg-transparent  border-0">
@@ -33,7 +47,12 @@ class Card extends React.Component{
                 </div>
               </div>
           </div>
-          <Modal index = {this.props.index}/>
+          <Modal
+            handleClose = {this.handleClose}
+            isopen = {this.state.isopen}
+            index = {this.props.index}
+            imagesrc = {this.props.src}
+          />
           </>
         )
     }
--- frontend/foodz/src/components/cards/promotionsCard/modal/index.js
+++ frontend/foodz/src/components/cards/promotionsCard/modal/index.js
@@ -1,40 +1,39 @@
 import React from "react"
-import 'bootstrap'
-import $ from 'jquery';
+import ReactModal from 'react-modal';
+import  './modal.css'
 
+const customStyles = {
+    section: {
+        marginTop : "1em"
+    }
+  };
+ReactModal.setAppElement('#root')
 class Modal extends React.Component{
-    constructor(props){
-        super(props)
-        this.handleClose = this.handleClose.bind()
-    }
-    handleClose = (event)=>{
-        let close = event.target.className.includes("close") ? event.target: event.target.parentNode.className.includes("close")  ? event.target.parentNode : undefined
-        if(close){
-            let id = "#modal-promotion-"+this.props.index
-            console.log($(id).modal("hide"))
-
-        }
-    }
+  
 
     render(){
-        return(
-            <div className="modal fade" id={"modal-promotion-"+this.props.index}  
-            tabIndex={"-1"}
-            aria-labelledby={"modal-promotion-"+this.props.index} 
-            aria-hidden="true">
-            <div className="modal-content">
-                <div className="modal-header">
-                <button type="button" className="close"  onClick={this.handleClose}  aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-                </div>
-                <div className="modal-body">
-                <p > 
-                  is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-                  </p>
-                </div>
-            </div>
-          </div>
+        return(  
+        <ReactModal  
+            isOpen={this.props.isopen}
+            onRequestClose={this.props.handleClose}
+            id= {"#modal-promotion-"+this.props.index} 
+             
+        >
+        
+        <div class="modal-body">
+       
+        <button  type="button" class="close" onClick={this.props.handleClose} aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+        </button>
+        <section >
+            <div>
+                <hr></hr>
+                <img className="img-fluid mt-2" src={this.props.imagesrc}></img>
+            </div>     
+        </section>
+        </div>
+        </ReactModal>
+         
         )
     }
 }
